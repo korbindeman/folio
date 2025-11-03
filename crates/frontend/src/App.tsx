f6c715ad79dbdd02a43bea38f86277dd77bd4ca1
Korbin de Man <korbindeman4@gmail.com>
2025-11-03 14:59:46 +0100

    label marking development mode

--- crates/frontend/src/App.tsx
+++ crates/frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Navigation } from "./components/Navigation";
 import EditorManager from "./components/EditorManager";
 
 function App() {
+  const isDev = import.meta.env.DEV;
+
   return (
     <NotesProvider>
       <div class="h-screen flex flex-col pt-0">
@@ -10,6 +12,11 @@ function App() {
         <div class="mt-8 flex flex-1">
           <EditorManager />
         </div>
+        {isDev && (
+          <div class="fixed bottom-2 left-2 opacity-30 text-xs font-bold z-50">
+            DEV
+          </div>
+        )}
       </div>
     </NotesProvider>
   );
